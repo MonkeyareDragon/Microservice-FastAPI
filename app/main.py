55995cd8df7a37ff315e34734f729f2dcbdc9811
MonkeyareDragon <githubforme73@gmail.com>
2024-06-24 12:55:51 +0545

    configure jinja in app.

---
+++ app/main.py
@@ -0,0 +1,17 @@
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+app = FastAPI()
+template = Jinja2Templates(directory=BASE_DIR / "templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    return template.TemplateResponse("home.html", {"request": request})
+
+@app.post("/")
+def home_detail_view():
+    return {"hello": "world"}
