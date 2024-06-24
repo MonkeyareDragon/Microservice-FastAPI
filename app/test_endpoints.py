1a86aa78f9802bf32170713825e2045bb9da8566
MonkeyareDragon <githubforme73@gmail.com>
2024-06-24 13:13:43 +0545

    FastAPI Git and pre-commit

--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -7,8 +7,8 @@ def test_home_view():
     response = client.get("/")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
-    
-    
+
+
 def test_post_view():
     response = client.post("/")
     assert response.status_code == 200
